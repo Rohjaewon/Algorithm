066d8b26137dcedb4383617924ecea0edb14b43f
Rohjaewon <rjwharry2003@gmail.com>
2020-10-11 19:00:47 +0900

    poly and dunibal

--- Algo/Dynamic/Dunibal.cpp
+++ Algo/Dynamic/Dunibal.cpp
@@ -1,68 +1,47 @@
-/*
- * Dunibal.cpp
- *
- *  Created on: 2019. 1. 13.
- *      Author: harry
- */
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
-int village, days, jail;
 
-void Get_P(vector<vector<int> > connect, vector<double>& probability, int num){
-	for(int i =0; i<num; i++){
-		probability[i] = (double)1/connect[i].size();
+int N = -1, d = -1, s = -1;
+int n;
+vector<double> village(52, 0.0);
+vector<double> connected(52, 0);
+vector<vector<int>> path(52, vector<int>(52, 0));
+
+void dunibal(int start, int d, double prob){
+	if(d == 0){
+		village[start] += prob;
+		return;
 	}
-}
-
-double path(vector<vector<int> > connect, vector<double> probability, int start, int finish, int current){
-	if(current == days){
-		if(start == finish){
-			return 1;
+	for(int i = 0; i < N; i++){
+		if(path[start][i] == 1){
+			dunibal(i, d-1, prob * (1 / connected[start]));
 		}
-		else return 0;
-	}
-	else{
-		vector<int>::iterator iter = connect[start].begin(); double answer = 0;
-		while(iter != connect[start].end()){
-			answer += probability[start]*path(connect, probability, *iter, finish, current+1);
-			iter++;
-		}
-		return answer;
-	}
-}
-
-void Dunibal(vector<vector<int> > connect,vector<double>& answer, vector<int> P){
-	vector<int> cnt(village, 0); vector<double> probability(village, 0);
-	Get_P(connect, probability, village);
-	for(unsigned int i =0; i<P.size(); i++){
-		answer[i] = path(connect, probability, jail, P[i], 0);
 	}
 }
 
 int main(){
-	cin >> village >> days>> jail;
-	vector<vector<int> > connect(village);
-	for(int i =0; i<village; i++){
-		for(int j =0; j<village; j++){
-			int temp; cin >> temp;
-			if(temp == 1)
-				connect[i].push_back(j);
+	
+	cin >> N >> d >> s;
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cin >> path[i][j];
 		}
 	}
-	int num; cin >> num;
-	vector<int> P(num, 0);
-	for(int i =0; i<num; i++){
-		int temp; cin >> temp;
-		P[i] = temp;
+	cin >> n;
+	vector<int> dist(n, -1);
+	for(int i = 0; i < n; i++)
+		cin >> dist[i]; 
+	for(int i = 0; i < N; i++){
+		int cnt = 0;
+		for(int j = 0; j < N; j++){	
+			if(path[i][j] == 1) cnt++;
+		}
+		connected[i] = cnt;
 	}
-	vector<double> answer(num, 0);
-	Dunibal(connect, answer, P);
-	for(int i =0; i< num; i++)
-		cout << answer[i] << " ";
-	return 0;
+	dunibal(s, d, 1);
+	for(int i = 0; i < n; i++){
+		cout << village[dist[i]] << " ";
+	} cout << endl;
 }
-
-
-
--- Algo/Dynamic/polyomino.cpp
+++ Algo/Dynamic/polyomino.cpp
@@ -1,79 +1,31 @@
-/*
- * polyomino.cpp
- *
- *  Created on: 2019. 1. 8.
- *      Author: harry
- */
 #include <iostream>
+#include <algorithm>
 #include <vector>
-using namespace std;
-const int MAX = 100;
-const long int mod = 10000000;
-int cache[MAX][MAX] = {0};
 
-int poly(int first, int num) {
-	int& temp = cache[first][num];
-	if(temp != 0) return temp;
-	if (num==0)
-		return temp=1;
-	else{
-	//cout << "first : " << first << " " << "num : " << num << endl;
-	for (int second = 1; second <= num; second++) {
-			temp += poly(second, num - second) * (first + second - 1);
-			temp %= mod;
-	}
-	//cout << temp << endl;
-	return temp;
-	}
-}
-
-int main() {
-	int num;
-	cin >> num;
-	vector<int> answer(MAX, 0);
-	for (int first = 1; first <= num; first++) {
-		//cout << "맨 위 : " << first << endl;
-		answer[first] = poly(first, num-first);
-	}
-	int sum = 0;
-	for (int i = 1; i <= num; i++){
-		sum += answer[i];
-		sum %= mod;
-	}
-	cout << sum << endl;
-}
-/*
-#include <iostream>
 using namespace std;
 
 const int MOD = 10000000;
-int cache[101][101];
+int N;
+vector<vector<int>> cache(102, vector<int>(102, -1));
 
-int poly(int n, int first){
-	if(n==first)
+int poly(int n, int prev){
+	if(n == 0){
 		return 1;
-	int &ret = cache[n][first];
-	if(ret != -1) return ret;
+	}
+	int& ret = cache[n][prev];
+	if(ret != -1 ) return ret;
 	ret = 0;
-	for(int second = 1; second <= n-first; ++second){
-		int add = second + first -1;
-		add *= poly(n-first, second);
-		add %= MOD;
-		ret += add;
+	for(int i = 1; i <=n; i++){
+		if(prev == 0)
+			ret += poly(n-i, i) % MOD;
+		else
+			ret += ((prev + i - 1) * poly(n-i, i))%MOD;
 		ret %= MOD;
 	}
 	return ret;
 }
 
 int main(){
-	int num; cin >> num; int answer = 0;
-	for(int i =0; i<101; i++){
-		for(int j = 0; j <101; j++)
-			cache[i][j] = -1;
-	}
-	for(int first = 1; first < num; first++){
-		answer += poly(num, first);
-		answer %= MOD;
-	}
-	cout << answer+1;
-}*/
+	cin >> N;	
+	cout << poly(N, 0) << endl;
+}
